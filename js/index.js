24e6ed0a52f57edc8c69df0b2ae14ca1102cb506
Ruben Pinto <rpinto@spartaglobal.com>
2018-10-10 23:42:25 +0100

    added a v2 of the code with improved object

--- js/index.js
+++ js/index.js
@@ -1,35 +1,102 @@
+// var num1;
+// var num2;
+// var choice = prompt ("Would you like to execute a regular or advanced calculation").toLowerCase();
+
+// var calculation = {
+//   add: "+",
+//   sub: "-",
+//   mul: "*",
+//   div: "/",
+//   pow: "pow",
+//   sroot: "sroot",
+// };
+
+//
+// if (choice=="regular") {
+//   var arithmetic = prompt ("what type of regular calculation would you like to do? (+,-,*,/)");
+//   switch (arithmetic) {
+//     case calculation.add:
+//       num1 = parseInt(prompt("What is the first number?"));
+//       num2 = parseInt(prompt("What is the second number?"));
+//       console.log(num1+num2);
+//       break;
+//     case calculation.sub:
+//       num1 = parseInt(prompt("What is the first number?"));
+//       num2 = parseInt(prompt("What is the second number?"));
+//       console.log(num1-num2);
+//       break;
+//     case calculation.mul:
+//       num1 = parseInt(prompt("What is the first number?"));
+//       num2 = parseInt(prompt("What is the second number?"));
+//       console.log(num1*num2);
+//       break;
+//     case calculation.div:
+//       num1 = parseInt(prompt("What is the first number?"));
+//       num2 = parseInt(prompt("What is the second number?"));
+//         if (num2==0) {
+//           console.log("operation not possible because you cannot divide by zero");
+//         }else {
+//           console.log(num/num2);
+//         }
+//       break;
+//     default:
+//       console.log("Wrong input. Try again!");
+//   }
+// }else {
+//   var arithmetic = prompt ("what type of advanced calculation would you like to do? (pow or sroot)");
+//   switch (arithmetic) {
+//     case calculation.pow:
+//       num1 = parseInt(prompt("What is the first number?"));
+//       num2 = parseInt(prompt("What is the power?"));
+//       console.log(Math.pow (num1, num2));
+//       break;
+//     case calculation.sroot:
+//       num1 = parseInt(prompt("What is the first number?"));
+//       console.log(Math.sqrt(num1));
+//       break;
+//       default:
+//       console.log ("Wrong input. try again!")
+//     }
+// }
+
+//v2.........................................
 var num1;
 var num2;
 var choice = prompt ("Would you like to execute a regular or advanced calculation").toLowerCase();
 
 var calculation = {
-  add: "+",
-  sub: "-",
-  mul: "*",
-  div: "/",
-  pow: "pow",
-  sroot: "sroot",
-};
+  regular: {
+    add: "+",
+    sub: "-",
+    mul: "*",
+    div: "/",
+  },
+  advanced: {
+    pow: "pow",
+    sroot: "sroot",
+  }
+}
+
 
 if (choice=="regular") {
   var arithmetic = prompt ("what type of regular calculation would you like to do? (+,-,*,/)");
   switch (arithmetic) {
-    case calculation.add:
+    case calculation.regular.add:
       num1 = parseInt(prompt("What is the first number?"));
       num2 = parseInt(prompt("What is the second number?"));
       console.log(num1+num2);
       break;
-    case calculation.sub:
+    case calculation.regular.sub:
       num1 = parseInt(prompt("What is the first number?"));
       num2 = parseInt(prompt("What is the second number?"));
       console.log(num1-num2);
       break;
-    case calculation.mul:
+    case calculation.regular.mul:
       num1 = parseInt(prompt("What is the first number?"));
       num2 = parseInt(prompt("What is the second number?"));
       console.log(num1*num2);
       break;
-    case calculation.div:
+    case calculation.regular.div:
       num1 = parseInt(prompt("What is the first number?"));
       num2 = parseInt(prompt("What is the second number?"));
         if (num2==0) {
@@ -44,12 +111,12 @@ if (choice=="regular") {
 }else {
   var arithmetic = prompt ("what type of advanced calculation would you like to do? (pow or sroot)");
   switch (arithmetic) {
-    case calculation.pow:
+    case calculation.advanced.pow:
       num1 = parseInt(prompt("What is the first number?"));
       num2 = parseInt(prompt("What is the power?"));
       console.log(Math.pow (num1, num2));
       break;
-    case calculation.sroot:
+    case calculation.advanced.sroot:
       num1 = parseInt(prompt("What is the first number?"));
       console.log(Math.sqrt(num1));
       break;
